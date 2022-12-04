93c6be805baeac2c4fe8d91715b6f9702c50ed0f
jpwiig <jpwiig@gmail.com>
2022-12-04 16:19:46 +0100

    fixing small things

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -10,7 +10,7 @@ public class Task1 {
         //b) while loop
         System.out.println("Task b");
         int j = 0; //initate outside of the while loop
-        while (j < 10000){
+        while (j < 1000){
             System.out.print(j + " ");// the code works downwards
             j+=10;
         }
@@ -18,8 +18,11 @@ public class Task1 {
         //c)
         System.out.println("Task c");
         int start = 10;
-        for (int i = start; i >= -10; i--){ //iterates backward
+        int end = 0;
+        for (int i = start; i > -10; i--){ //iterates backward
             System.out.print(i + ", ");
+            end = i;
         }
+        System.out.print(end - 1); //lazy mans solution
     }
 }
